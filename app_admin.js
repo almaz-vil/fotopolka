803ca442fd8a405318c5f44e3af351c1980d3359
almaz-vil <37326184+almaz-vil@users.noreply.github.com>
2022-12-13 15:17:16 +0300

    Update app_admin.js

--- app_admin.js
+++ app_admin.js
@@ -4,14 +4,14 @@ console.log('Мы производим, очистку базы данный и 
 const Admin = require('./admin');
 const sqlite =require('sqlite-sync');// require("sqlite3");
 sqlite.connect('foalbom');
-var p = new Promise(function (resolve, reject){
-    var admin = new Admin(sqlite);
-    resolve(admin.add_ixef());
-})
-p.then(function (){
-    console.log('Завершения работы системы !!!\n');
+//var p = new Promise(function (resolve, reject){
+    //var admin = new Admin(sqlite);
+  //  resolve(admin.add_ixef());
+//})
+//p.then(function (){
+  //  console.log('Завершения работы системы !!!\n');
 
-})
+//})
 
-//admin.clear_base();
-//admin.scan_dir();
+admin.clear_base();
+admin.scan_dir();
